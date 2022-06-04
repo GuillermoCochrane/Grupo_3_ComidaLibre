71ab5b6fc5a97922de0c96ab0bc544511fb7753d
arielmurua <arielmur33@gmail.com>
2022-06-03 21:52:00 -0300

    estructura basica

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const index = express();
+const path = require('path');
+
+index.use('/static', express.static(path.resolve(__dirname,'./public')));
+index.use('/images', express.static(path.resolve(__dirname,'./public/images')));
+index.use('/css', express.static(path.resolve(__dirname,'./public/css')));
+index.use('/views', express.static(path.resolve(__dirname,'./views')));
+
+
+index.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+});
+index.get('/products', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+});
+index.get('/cart', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/productCart.html'))
+});
+index.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+index.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+
+
+index.listen(3000, () => {
+    console.log('Server up')
+})
