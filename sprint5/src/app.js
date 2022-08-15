f7ce0eb68a4c9a3e81f33ea2a54f007c620a2679
arielmurua <arielmur33@gmail.com>
2022-08-15 17:10:59 -0300

    Agregado de usuarios admins y normales

--- sprint5/src/app.js
+++ sprint5/src/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const rememberUserMiddleware = require('./middlewares/rememberUserMdw');
+const rememberUserMdw= require('./middlewares/rememberUserMdw');
 
 //EXPRESS
 const app = express();
